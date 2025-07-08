6021429f0b2eb8c3a7e0344b92d9e85b727f7b43
reddogninja <159635263+reddogninja@users.noreply.github.com>
2025-07-08 15:10:20 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,41 @@
 import pygame
-from Config import *
+from config import *
 
 #Init
 pygame.init()
-clock=pygame.time.Clock()
+clock = pygame.time.Clock()
 
 # Open window
 displaySurface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Parasite")
+
+button_images = [
+    pygame.image.load('assets/Heart1.png'),
+    pygame.image.load('assets/Heart2.png'),
+    pygame.image.load('assets/Heart3.png'),
+
+]
+
+for i in range(len(button_images)):
+    button_images[i] = pygame.transform.smoothscale(button_images[i], (BUTTON_RADIUS*2, BUTTON_RADIUS*2))
+
+class CircularButton:
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (80, 80, 80), self.rect.center, BUTTON_RADIUS)
+        surface.blit(self.image, self.rect)
+
+
+buttons = []
+for i in range(3):
+    x = WINDOW_WIDTH - BUTTON_RADIUS - 20
+    y = (BUTTON_RADIUS + BUTTON_SPACING) * i + BUTTON_RADIUS + 40
+    buttons.append(CircularButton(x, y, button_images[i]))
+
+
 
 isGameRunning = True
 while isGameRunning:
@@ -15,9 +43,15 @@ while isGameRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isGameRunning = False
-        elif event.type ==pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+        elif event.type ==pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 isGameRunning = False
+
+    displaySurface.fill((20, 20, 30))
+
+    for button in buttons:
+        button.draw(displaySurface)
+
+
     pygame.display.flip()
     clock.tick(60)
 
